4518737098410251cd47c5b044c7503e074c608b
François Bobot <francois.bobot@cea.fr>
2025-10-13 22:32:41 +0200

    add vega and data export

--- static_page/charts.py
+++ static_page/charts.py
@@ -13,6 +13,9 @@ import common_charts
 import typer
 from pathlib import Path
 from rich.progress import track
+import vl_convert
+import altair.utils._importers
+import json
 
 app = typer.Typer()
 
@@ -22,11 +25,13 @@ env = Environment(
 )
 
 @app.command()
-def html(database:Path,folder:Path,logic:list[str]=[],details:bool=False,virtual:bool=False,par4:bool=False,dist_too_few:float|None=None,min_common_benches:int=100,isomap:bool=False,euclidean:bool=False,html:bool=True,pdf:bool=False,png:bool=False,index:bool=True):
+def html(database:Path,folder:Path,logic:list[str]=[],details:bool=False,virtual:bool=False,par4:bool=False,dist_too_few:float|None=None,min_common_benches:int=100,isomap:bool=False,euclidean:bool=False,html:bool=True,pdf:bool=False,png:bool=False,vega:bool=False,data:bool=False,index:bool=True):
 
     os.makedirs(f"{folder}/isomap",exist_ok=True)
     os.makedirs(f"{folder}/isomap/pdf",exist_ok=True)
     os.makedirs(f"{folder}/isomap/png",exist_ok=True)
+    os.makedirs(f"{folder}/isomap/vega",exist_ok=True)
+    os.makedirs(f"{folder}/isomap/data",exist_ok=True)
     charts_template = env.get_template("isomap.html")
     
     if len(logic) == 0:
@@ -40,22 +45,32 @@ def html(database:Path,folder:Path,logic:list[str]=[],details:bool=False,virtual
     for l in track(logic):
         try:
             r = common_charts.compute_charts(l,details,virtual,dist_too_few,min_common_benches,par4,isomap,euclidean,database)
-
-            if html:
-                charts_template.stream(
-                    logicData=l,
-                    printed="",
-                    charts=r["all"].to_html(fullhtml=False),
-                    show_form=False,
-                    inputs_value=r,
-                ).dump(str(folder / "isomap" / f"{l}.html"))
-            
-            if pdf:
-                r["all"].save(fp=folder / "isomap" / "pdf" / f"{l}.pdf",format="pdf")
-            if png:
-                r["all"].save(fp=folder / "isomap" / "png" / f"{l}.png",format="png")
         except Exception as e:
             print (f"Error during conversion of {l}:",e)
+
+        if html:
+            charts_template.stream(
+                logicData=l,
+                printed="",
+                charts=r["all"].to_html(fullhtml=False),
+                show_form=False,
+                inputs_value=r,
+            ).dump(str(folder / "isomap" / f"{l}.html"))
+        
+        if pdf:
+            r["all"].save(fp=folder / "isomap" / "pdf" / f"{l}.pdf",format="pdf")
+        if png:
+            r["all"].save(fp=folder / "isomap" / "png" / f"{l}.png",format="png")
+        if vega:
+            vega_spec=vl_convert.vegalite_to_vega(vl_spec=r["all"].to_dict(context={"pre_transform": False}),vl_version=altair.utils._importers.vl_version_for_vl_convert())
+            (folder / "isomap" / "vega" / f"{l}.json").write_text(json.dumps(
+            vega_spec,indent=2))
+        if data:
+            vega_spec=vl_convert.vegalite_to_vega(vl_spec=r["all"].to_dict(context={"pre_transform": False}),vl_version=altair.utils._importers.vl_version_for_vl_convert())
+            vega_spec={"data" : vega_spec["data"][2]["values"],
+                       "domain" : vega_spec["scales"][2]["domain"],}
+            (folder / "isomap" / "data" / f"{l}.json").write_text(json.dumps(
+            vega_spec,indent=2))
 
 if __name__ == "__main__":
     app()
